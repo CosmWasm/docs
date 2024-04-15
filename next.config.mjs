6382a0887104394ff65a22eb0aa067fe2e898d7d
abefernan <44572727+abefernan@users.noreply.github.com>
2024-04-15 13:32:35 +0200

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./src/theme.config.js",
+  latex: true,
+  flexsearch: {
+    codeblock: false,
+  },
+});
+
+export default withNextra({
+  reactStrictMode: true,
+});
